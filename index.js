70d2437a40a8083b2da307f05b323dc9053d9273
Mauricio Sarmiento <micho.sarmiento.b@gmail.com>
2018-03-16 16:36:18 -0700

    javascript working

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ let element = document.getElementById('status');
 let counter = document.getElementById("show");
 
 function countdown(minutes) {
-  let seconds = 60;
+  let sec = 60;
   let min = minutes;
 
   function countMin() {    
     let currentMin = min-1;
-    seconds--;
-    console.log(min, seconds);
-    counter.innerHTML = currentMin.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
-    if(min === 1 && seconds === 0){
+    sec--;
+    console.log(min, sec);
+    counter.innerHTML = currentMin.toString() + ":" + (sec < 10 ? "0" : "") + String(sec);
+    if(min === 1 && sec === 0){
       element.innerHTML = "Done, bye";
-    } else if( seconds > 0 ) {
+    } else if( sec > 0 ) {
       setTimeout(countMin, 100);
     } else {
       if(currentMin === 1){
